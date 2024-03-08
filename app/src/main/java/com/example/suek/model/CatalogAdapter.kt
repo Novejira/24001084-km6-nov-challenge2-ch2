3dc75cd28ce608207a96bc3c93fd1cd42f36aa82
Novejira Angela <156919887+Novejira@users.noreply.github.com>
2024-03-09 04:35:51 +0700

    finishing feature rv catalog

---
+++ app/src/main/java/com/example/suek/model/CatalogAdapter.kt
@@ -0,0 +1,44 @@
+package com.catnip.layoutingexample
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.suek.databinding.ItemCatalogBinding
+import com.example.suek.model.Catalog
+
+class CatalogAdapter : RecyclerView.Adapter<CatalogAdapter.CatalogViewHolder>() {
+
+    private val data = mutableListOf<Catalog>()
+
+    fun submitData(items: List<Catalog>) {
+        data.addAll(items)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatalogViewHolder {
+        return CatalogViewHolder(
+            ItemCatalogBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    //counting the data size
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: CatalogViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    class CatalogViewHolder(private val binding: ItemCatalogBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Catalog) {
+            binding.tvCatalogName.text = item.name
+            binding.ivCatalogImage.setImageResource(item.image)
+            binding.tvCatalogPrice.text = item.price.toString()
+        }
+    }
+}
+
+
